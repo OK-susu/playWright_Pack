7e82a1115a0621c589d6c1c6ed91d8809dd377e8
InJae <108115109+OK-susu@users.noreply.github.com>
2022-11-14 22:44:29 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// const ConsoleWindow = require("node-hide-console-window");
+//To hide your console just call:
+// ConsoleWindow.hideConsole(); <- cmd 창 지우기 
+// playwright 1.23.0
+const { chromium } = require('playwright');
+const common_Result = require(__dirname + "/File_js/common_Result.js");
+
+function IsJsonString(str) {
+  try {
+    inputJson = JSON.parse(str);
+    return (typeof inputJson === 'object');
+  } catch (e) {
+    return false;
+  }
+}
+
+function myPlayWright () {
+  const base64Encoded = process.argv[2];
+
+  if (!base64Encoded) {
+    return common_Result.prototype._Result("8000F200");
+  }
+  console.log("1. Base64: " + base64Encoded);
+
+  const parameters = Buffer.from(base64Encoded, "base64").toString('utf8');
+  console.log("2. PlainText: " + parameters);
+
+  if (!IsJsonString(parameters)) { //Json 형식이 아님으로 종료
+    return common_Result.prototype._Result("8000F100");
+  }
+
+  var json = JSON.parse(parameters);
+  const moduleName = json['FileName'];
+  const jobName = json['jobName'];
+  const ModuleSite = require(__dirname + "/File_js/" + moduleName);
+
+  console.log("JS 실행");
+  ModuleSite.prototype[jobName](chromium, parameters).then (function (data) {
+      console.log("obJect data: " + data);
+      console.log("String data: " + JSON.stringify(data) );
+  });
+      
+}
+
+myPlayWright();
